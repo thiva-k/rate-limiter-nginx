eb62053b14ab4427188aeb06d2ee04e8bafa2a89
tharsigan <tharsigan25@gmail.com>
2024-10-03 12:07:48 +0530

    refactor sliding window sql

--- init/init_sliding_window_counter.sql
+++ init/init_sliding_window_counter.sql
@@ -4,10 +4,9 @@ ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 -- Use the rate_limit_db
 USE rate_limit_db;
 
--- Create the sliding window table with improvements
-CREATE TABLE rate_limit_sliding_window (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing primary key
-    token VARCHAR(255) NOT NULL,           -- Token for each user/client
-    request_time DOUBLE NOT NULL,          -- Store request time in seconds
-    INDEX (token, request_time)            -- Index for fast querying by token and time
+CREATE TABLE sliding_window_counter (
+    token VARCHAR(255) NOT NULL,
+    bucket INT NOT NULL, -- Stores the time bucket, based on granularity
+    request_count INT DEFAULT 0, -- Counter for the number of requests in the bucket
+    PRIMARY KEY (token, bucket)
 );
