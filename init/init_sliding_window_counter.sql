b24d00a784ba2fcfaebd134c98f54f6aaf42c850
tharsigan <tharsigan25@gmail.com>
2024-10-05 12:30:14 +0530

    refactor sliding window counter sql schema

--- init/init_sliding_window_counter.sql
+++ init/init_sliding_window_counter.sql
@@ -4,9 +4,8 @@ ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 -- Use the rate_limit_db
 USE rate_limit_db;
 
-CREATE TABLE sliding_window_counter (
-    token VARCHAR(255) NOT NULL,
-    bucket INT NOT NULL, -- Stores the time bucket, based on granularity
-    request_count INT DEFAULT 0, -- Counter for the number of requests in the bucket
-    PRIMARY KEY (token, bucket)
+CREATE TABLE request_limits (
+    token VARCHAR(255) PRIMARY KEY,
+    last_access DATETIME,
+    sub_windows JSON
 );
