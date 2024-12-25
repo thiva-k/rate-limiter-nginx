11ca04d3475015efb8f8977d6ed847c265692c89
tharsigan <tharsigan25@gmail.com>
2024-12-25 07:19:09 +0530

    sliding window counter sql init added

--- init/init_sliding_window_counter.sql
+++ init/init_sliding_window_counter.sql
@@ -3,9 +3,9 @@ ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 
 -- Use the rate_limit_db
 USE rate_limit_db;
-
-CREATE TABLE request_limits (
-    token VARCHAR(255) PRIMARY KEY,
-    last_access DATETIME,
-    sub_windows JSON
+CREATE TABLE rate_limit_requests (
+    token VARCHAR(255) NOT NULL,
+    subwindow BIGINT NOT NULL,
+    request_count INT DEFAULT 0,
+    PRIMARY KEY (token, subwindow)
 );
